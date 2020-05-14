b0f24d28c7ff2355e3cb14386fb657e2f6df0207
Ian Dela Cruz <iandc76@gmail.com>
2020-05-14 20:06:54 +0800

    Rename test folder

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['/node_modules/', '/__tests__/__helpers'],
+  testPathIgnorePatterns: ['/node_modules/', '/tests/__helpers'],
   collectCoverage: true,
   coverageReporters: ['text', 'lcov'],
   collectCoverageFrom: ['src/**/*.js'],
