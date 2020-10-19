b23d9ce53913b991bfc8e0a391bd4499116027be
Ian Dela Cruz <iandc76@gmail.com>
2020-10-19 19:04:53 +0800

    Switch to rollup

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+import visualizer from 'rollup-plugin-visualizer';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    dir: 'dist',
+    format: 'cjs',
+    exports: 'default',
+  },
+  plugins: [resolve(), typescript(), commonjs(), terser(), visualizer()],
+};
